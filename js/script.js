fbc158faf87d9b0a50c87690f8d3e80d3a8d71ba
Jacob Yoon <jacobyoon926@gmail.com>
2018-04-23 20:30:26 -0400

    Add footer, other small edits

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ var elementPosition = $('.h').offset();
 $(window).scroll(function(){
   if($(window).scrollTop() > elementPosition.top){
     $('.h').css('position','fixed').css('top','100px').css('box-shadow','0 0 16px rgba(0,0,0,0.7)');
+    $('.designbriefhome').css('color','white').css('border', '1px solid white');
   } else {
     $('.h').css('position','absolute').css('top','100vh').css('box-shadow','0 0 16px rgba(0,0,0,0)');
+    $('.designbriefhome').css('color','var(--secondarycolor)').css('border', '1px solid var(--secondarycolor)');
   }
 });
 
